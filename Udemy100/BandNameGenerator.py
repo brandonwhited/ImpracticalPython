93d4c3889c65cc6ea2f87e7a8e3f5b338bbe2a42
brandonwhited <brandonwhited12@gmail.com>
2024-02-28 17:15:05 -0500

    Band name Generator

---
+++ Udemy100/BandNameGenerator.py
@@ -0,0 +1,6 @@
+# Band Name Generator 
+print('Welcome to the Band Name Generator\n')
+City = input(print("What's the name of the city you grew up in?\n"))
+Month = input(print( "Great, what month we're you born?\n"))
+
+print("You're band name is " + City + " t" + Month)
--- Udemy100/Day1.py
+++
@@ -1,6 +0,0 @@
-
-input("What is your name?/n")
-
-print('"Welcome to the Band Name Generator"\n')
-print("What's the name of the city you grew up in?")
-
