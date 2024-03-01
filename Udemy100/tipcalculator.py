b7d3339cdee1e110d44f96f60fe2e990799c6087
brandonwhited <brandonwhited12@gmail.com>
2024-03-01 13:31:10 -0500

    Making more progress

--- Udemy100/tipcalculator.py
+++ Udemy100/tipcalculator.py
@@ -1,11 +1,13 @@
 print("Welcome to the Tip Calculator")
 
 Total = float(input("What is the total amount of the bill?\n"))
-Split_Between = float(input("How many people will be splitting the bill?\n"))
+Split_Between = int(input("How many people will be splitting the bill?\n"))
 Tip = float(input("What % tip do you wish to leave? EX: 10, 12, 15 or 20%\n"))
 
 Adapted_Tip = Tip / 100 
 
-Final_Product = ((Total * Adapted_Tip) / Split_Between)
-Converted_Final_Product = str(Final_Product)
-print("Each person shall pay " + Converted_Final_Product + " dollars")
+
+Final_Product = float(((Total * Adapted_Tip) + Total) / Split_Between)
+
+
+print(f"Each person shall pay {Final_Product} dollars")
