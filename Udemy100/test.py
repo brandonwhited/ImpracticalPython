acc690422439e61ca73bd37d924c9780d5e224f3
brandonwhited <brandonwhited12@gmail.com>
2024-03-04 15:55:26 -0500

    This is getting harder

--- Udemy100/test.py
+++
@@ -1,8 +0,0 @@
-names_string = ["James, John, Ben"] 
-
-names = names_string.split("James, John, Ben ")
-# The code above converts the input into an array seperating
-#each name in the input by a comma and space.
-# 🚨 Don't change the code above 👆
-
-print(len(names))
