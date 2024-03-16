db2b1253ac377ea93fb37c94b7c164e06617ab0b
brandonwhited <brandonwhited12@gmail.com>
2024-03-15 23:12:12 -0400

    More progress

--- Udemy100/hangman.py
+++ Udemy100/hangman.py
@@ -4,15 +4,33 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 chosen_letters = list(chosen_word)
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-User_Input = input("Guess a letter: ")
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-for letter in chosen_letters:
-    if letter ==  User_Input:
-        print("Right")
-    else:
-        print("False")
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+display = []
+for _ in chosen_letters:
+    display.append("_")
+# OR display += "_"
+# OR display = ["_" for _ in chosen_letters]
+# OR for _ in range(len()chosen_word): display += "_"
+
+#TODO-2: - Loop through each position in the chosen_word;
+
+guess = input("Guess a letter: ").lower()
+
+for i, letter in enumerate(chosen_letters):
+    if letter == guess:
+        display[i] = guess
         
+# OR for position in range(len(chosen_word)):
+#       letter = chosen_word[position]
+#       if letter == guess:
+#           display[position] = letter
+
+
+print(display)
