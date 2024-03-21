f41e3b4cd56a39c7d6666044a8c7f2978158f748
brandonwhited <brandonwhited12@gmail.com>
2024-03-21 18:36:05 -0400

    Functions with arguments

---
+++ Udemy100/Day8.py
@@ -0,0 +1,14 @@
+
+def greeting():
+    print("Hello, world!")
+    print("How are you diong")
+    print("YOU BETTER ANMSWER ME")
+    
+    
+def greet_with(name, location):
+    print(f"Hello, {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Tim", "London")
+
+
